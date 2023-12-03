e8f3265a5c4a971679cecf2569f5ccdc2e51c035
Hizin <adjizan@gmail.com>
2023-12-03 14:07:42 +0100

    FEAT : setting up architecture

--- app/src/main/java/com/cyberelephant/bank/presentation/FirstFragment.kt
+++ app/src/main/java/com/cyberelephant/bank/presentation/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.cyberelephant.bank
+package com.cyberelephant.bank.presentation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.cyberelephant.bank.R
 import com.cyberelephant.bank.databinding.FragmentFirstBinding
 
 /**
--- app/src/main/java/com/cyberelephant/bank/presentation/MainActivity.kt
+++ app/src/main/java/com/cyberelephant/bank/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cyberelephant.bank
+package com.cyberelephant.bank.presentation
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.cyberelephant.bank.R
 import com.cyberelephant.bank.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
